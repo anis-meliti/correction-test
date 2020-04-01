c1675799953da6129779d040b7ae892877d7d73a
Anis Melliti <ansmeliti@gmail.com>
2020-04-01 16:13:04 +0100

    fix the path

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <BrowserRouter>
           <Route exact path='/' render={() => <Users />} />
           <Route
-            exact
+            // exact
             path='/user/:id'
             render={props => <Profile {...props} />}
           />
